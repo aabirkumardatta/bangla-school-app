c1e3a4da35d67c63fbdcc2bbeab20f91cd0e89da
Aabir Kumar Datta <aabirkumardatta@yahoo.com>
2023-09-10 14:53:53 -0400

    feature/mojaru-v2: Reading Word Hope Page. Group 8-14 most recent videos from Baban Da included.

--- src/assets/ReadingAssets/Assets/ReadingVideos/Lesson9/readingLetterLesson9JsonFile.js
+++ src/assets/ReadingAssets/Assets/ReadingVideos/Lesson9/readingLetterLesson9JsonFile.js
@@ -1,7 +1,9 @@
 import video1 from "./video1.mp4";
+import video2 from "./video2.mp4";
 
 var readingLetterLesson9VideosArray = [
-    { previousButtonVisibility: "hidden", nextButtonVisibility: "hidden", previousObjectPosition: null, nextObjectPosition: null, previousVideo: null, currentVideo: video1, nextVideo: null}
+    { previousButtonVisibility: "hidden", nextButtonVisibility: "visible", previousObjectPosition: null, nextObjectPosition: 1, previousVideo: null, currentVideo: video1, nextVideo: video2},
+	{ previousButtonVisibility: "visible", nextButtonVisibility: "hidden", previousObjectPosition: 0, nextObjectPosition: null, previousVideo: video1, currentVideo: video2, nextVideo: null}
 ]
 
 export default readingLetterLesson9VideosArray;
